cd59e7e5749f686c2cbce83775e4ccf4847ef45d
Roman Smirnov <s.roman3455@gmail.com>
2024-10-28 01:01:18 +0300

    Fix Hexlet project check

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
-    Validator validator = new Validator();
+    public Validator validator = new Validator();
 
     @Test
     void testStringSchema() {
